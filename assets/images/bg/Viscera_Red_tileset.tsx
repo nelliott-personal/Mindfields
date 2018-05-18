39ca2d91dd4abd0b457881e3b34b771b6119d677
Nick Elliott <nelliott@liquidus.net>
2018-05-18 12:19:45 -0500

    moving files

---
+++ assets/images/bg/Viscera_Red_tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Viscera_Red_tileset" tilewidth="32" tileheight="32" tilecount="900" columns="50">
+ <image source="viscera_transparent.png" width="1600" height="600"/>
+</tileset>
